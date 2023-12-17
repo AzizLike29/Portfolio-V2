54bf668d132cef72598bde559ad1fd7f86af51e9
AzizLike29 <abdulfirdaus590@gmail.com>
2023-12-17 22:30:03 +0700

    update more

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,26 +1,93 @@
-function sendMessage() {
-  (function () {
-    emailjs.init("2G0rRaz7fr-5SKSgQ"); // Account Public Key
-  })();
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
 
-  var serviceID = "service_dlu2zjg"; // Email Service ID
-  var templateID = "template_3wsxnll"; // Email Template ID
+function sendEmail() {
+  const bodyMessage = `name: ${fullName.value}<br> email: ${email.value}<br> subject: ${subject.value}<br> message: ${message.value}`;
 
-  var params = {
-    sendername: document.querySelector("#name").value,
-    senderemail: document.querySelector("#email").value,
-    subject: document.querySelector("#subject").value,
-    message: document.querySelector("#message").value,
-  };
-
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      alert(
-        "Thank you, " + params["sendername"] + "! Your message has been sent."
-      );
-    })
-    .catch((error) => {
-      console.error("Error sending email:", error);
-    });
+  Email.send({
+    SecureToken: "05aba697-c1e5-412c-bc5e-331f8c07fd09",
+    Username: "abdulfirdaus590@gmail.com",
+    Password: "A2CD8030A38E8E7FCE1E1CDD3CEAB90FA9BB",
+    To: "abdulfirdaus590@gmail.com",
+    From: "abdulfirdaus590@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success!",
+        text: "The message has been successfully dispatched!",
+        icon: "success",
+      });
+    }
+  });
 }
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+function checkEmail() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+  const errorTxtEmail = document.querySelector(".error-txt.email");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+
+    if (email.value != "") {
+      errorTxtEmail.innerText = "Enter a valid email address";
+    } else {
+      errorTxtEmail.innerText = "Email can't be blank";
+    }
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (
+    !fullName.classList.contains("error") &&
+    !email.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !message.classList.contains("error")
+  ) {
+    sendEmail();
+
+    form.reset();
+    return false;
+  }
+});
